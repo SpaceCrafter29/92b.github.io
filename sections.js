699e0b81727ab5d9fc90b1b4e911fa607fe6de43
mschro67 <maximilian.s107@evt.schule.koeln>
2026-01-23 16:29:19 +0000

    Remake section system + Rename AI files

---
+++ sections.js
@@ -0,0 +1,31 @@
+//by mschro67
+//last change: Jan 23 2026
+
+const sections=document.getElementsByTagName("section");
+
+function showAll(){
+    for (const section of sections){
+        if (section.hasAttribute("hidden")){
+            section.removeAttribute("hidden");
+        }
+    }
+}
+
+function hideAll(){
+    for (const section of sections){
+        if (!section.hasAttribute("hidden")){
+            section.toggleAttribute("hidden");
+        }
+    }
+}
+
+function show(selected){
+    hideAll();
+    document.getElementById(selected).toggleAttribute("hidden");
+
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
